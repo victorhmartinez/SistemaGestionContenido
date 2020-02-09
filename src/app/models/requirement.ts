d7e5fb317f541ee697fcea7ea976206aca55f400
victormartinez <victormartinezg1998@gmail.com>
2020-02-08 23:17:47 -0500

    malla

--- src/app/models/requirement.ts
+++ src/app/models/requirement.ts
@@ -2,10 +2,10 @@ import { SubjectMatter } from './subject-matter';
 
 export class Requirement {
     requirement_id: number;
-    subject_matter_id : SubjectMatter;
-    subject_matter_requeriment_id :SubjectMatter;
+    subject_matter_id : number;
+    subject_matter_requeriment_id :number;
 
-    constructor(requirement_id ?:number, subject_matter_id?: SubjectMatter,subject_matter_requeriment_id?:SubjectMatter  ){
+    constructor(requirement_id ?:number, subject_matter_id?: number,subject_matter_requeriment_id?:number  ){
         this.requirement_id= requirement_id;
         this.subject_matter_id = subject_matter_id;
         this.subject_matter_requeriment_id = subject_matter_requeriment_id
